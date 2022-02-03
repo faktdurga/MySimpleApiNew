fdfd477eed1a9b90fe2f7e52dff71bfa16daf78e
faktdurga <mohited24@gmail.com>
2022-02-03 16:26:35 +0530

    Add project files.

---
+++ MySimpleApi/Model/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySimpleApi.Model
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGluntenFree { get; set; }
+    }
+}
